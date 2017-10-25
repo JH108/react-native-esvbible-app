9b799ea860fab61e2d22fe74ef4ecc09ea09720c
Jesse Hill <jsnahill@gmail.com>
2017-10-25 18:40:52 -0500

    Add menu icons and import all components into main app file

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Header from './Header';
+import Homepage from './Homepage';
+import Footer from './Footer';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.overlay}>
-          <Text style={styles.heavens}>
-            The heavens declare the glory of God, and the sky above proclaims his handiwork.
-          </Text>
-          <Image source={require('./fullsizeoutput_155.png')} />
-        </View>
+        <Header />
+        <Homepage />
+        <Footer />
       </View>
     );
   }
@@ -19,23 +19,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    margin: 0,
-  },
-  overlay: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(253, 183, 73, .15),'
-  },
-  heavens: {
-    alignSelf: 'center',
-    textAlign: 'center',
-    color: '#44aa66',
-    zIndex: 1,
-    position: 'absolute',
-    fontWeight: 'bold',
   },
 });

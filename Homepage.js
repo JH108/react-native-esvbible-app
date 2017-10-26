c9185146227ac1f470f8ac44781d3683bfb5ebcd
Jesse Hill <jsnahill@gmail.com>
2017-10-26 09:49:02 -0500

    Add react navigator

--- Homepage.js
+++ Homepage.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
+import Footer from './Footer';
 
 export default class Homepage extends React.Component {
   render() {
     return (
-      <View style={styles.overlay}>
-        <Text style={styles.heavens}>
-          The heavens declare the glory of God, and the sky above proclaims his handiwork.
-        </Text>
-        <Image source={require('./fullsizeoutput_155.png')} />
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.overlay}>
+          <Text style={styles.heavens}>
+            The heavens declare the glory of God, and the sky above proclaims his handiwork.
+          </Text>
+          <Image source={require('./fullsizeoutput_155.png')} />
+        </View>
+        <Footer />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
   overlay: {
     flex: 7,
     justifyContent: 'center',
